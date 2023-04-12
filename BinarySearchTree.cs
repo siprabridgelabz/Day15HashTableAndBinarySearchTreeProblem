a147f557ed4d50d615f1254128ff9de39c1256b9
sipra <sipramishra085@gmail.com>
2023-04-13 00:59:39 +0530

    Search Method And recursively search left or right

--- BinarySearchTree.cs
+++ BinarySearchTree.cs
@@ -59,5 +59,31 @@ namespace BinarySearchTreeProblem
             Console.WriteLine("Size" + " " + (7 * (this.leftCount + this.rightCount) - 1));
         }
 
+        public bool IfExists(T element, BinarySearchTree<T> node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.NodeData.Equals(element))
+            {
+                Console.WriteLine("Found the element in BST" + " " + node.NodeData);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Current element is {0} in BST", node.NodeData);
+            }
+            if (element.CompareTo(node.NodeData) < 0)
+            {
+                IfExists(element, node.LeftTree);
+            }
+            if (element.CompareTo(node.NodeData) > 0)
+            {
+                IfExists(element, node.RightTree);
+            }
+            return result;
+        }
+
     }
 }
