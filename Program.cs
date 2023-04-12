1bb34b4b9d5c93593010cf8081fec7b9c0a74b82
sipra <sipramishra085@gmail.com>
2023-04-13 00:51:47 +0530

    Added UC2

--- Program.cs
+++ Program.cs
@@ -9,7 +9,19 @@
             BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
             binarySearch.Add(30);
             binarySearch.Add(70);
+            binarySearch.Add(22);
+            binarySearch.Add(40);
+            binarySearch.Add(60);
+            binarySearch.Add(95);
+            binarySearch.Add(11);
+            binarySearch.Add(65);
+            binarySearch.Add(3);
+            binarySearch.Add(16);
+            binarySearch.Add(63);
+            binarySearch.Add(67);
             binarySearch.Display();
+            binarySearch.GetSize();
+
 
         }
     }
