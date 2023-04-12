fec690a5aa1b32d76aed87dc9e5a9407673ec445
sipra <sipramishra085@gmail.com>
2023-04-13 00:39:35 +0530

    Added Program File

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BinarySearchTreeProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("*****Welcome to the Hash Table and Binary Search Tree  problem*****");
+
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Add(30);
+            binarySearch.Add(70);
+            binarySearch.Display();
+
+        }
+    }
+}
