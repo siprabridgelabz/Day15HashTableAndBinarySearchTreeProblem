f9ef3da7da8543e135ecc092a5c928faa950f563
sipra <sipramishra085@gmail.com>
2023-04-13 00:45:43 +0530

    Insert the value in tree

---
+++ BinarySearchTree.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeProblem
+{
+   
+    class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BinarySearchTree<T> LeftTree { get; set; }
+        public BinarySearchTree<T> RightTree { get; set; }
+        public BinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+
+        }
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+
+        public void Add(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new BinarySearchTree<T>(item);
+                else
+                    this.LeftTree.Add(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BinarySearchTree<T>(item);
+                else
+                    this.RightTree.Add(item);
+            }
+        }
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+
+    }
+}
